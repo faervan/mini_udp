b109ced23a0df6f87ae66d85c19ac25a91990fa6
faervan <faervan@proton.me>
2026-06-12 23:28:09 +0200

    chore: remove unused test it_compiles

--- mini_udp/src/lib.rs
+++ mini_udp/src/lib.rs
@@ -10,53 +10,6 @@ pub use mini_udp_derive::BitRepr;
 mod test {
     use super::*;
 
-    // #[test]
-    // fn it_compiles() {
-    //     #[derive(BitRepr, PartialEq, Debug)]
-    //     enum X {
-    //         Y(u32),
-    //         Z,
-    //     }
-    //     #[derive(BitRepr)]
-    //     enum Y {
-    //         Z,
-    //     }
-    //     #[derive(BitRepr)]
-    //     enum Z {
-    //         A,
-    //         B,
-    //         C,
-    //     }
-    //     #[derive(BitRepr)]
-    //     enum ZX {
-    //         A,
-    //         B,
-    //         C,
-    //         D,
-    //     }
-    //     #[derive(BitRepr)]
-    //     enum ZY {
-    //         A,
-    //         B,
-    //         C,
-    //         D,
-    //         E,
-    //     }
-    //     assert_eq!(X::Y(0).bit_len(), 1);
-    //     assert_eq!(Y::Z.bit_len(), 0);
-    //     assert_eq!(X::from_bytes(&[1; 1], 0).unwrap(), X::Z);
-    //     // #[derive(BitRepr)]
-    //     // enum Z {}
-    //     // Z::from_bytes(&[0; 0], 0).unwrap();
-    //
-    //     assert_eq!(Z::MIN_BIT_LEN, Z::MAX_BIT_LEN);
-    //     assert_eq!(Z::MIN_BIT_LEN, 2);
-    //     assert_eq!(ZX::MIN_BIT_LEN, ZX::MAX_BIT_LEN);
-    //     assert_eq!(ZX::MIN_BIT_LEN, 2);
-    //     assert_eq!(ZY::MIN_BIT_LEN, ZY::MAX_BIT_LEN);
-    //     assert_eq!(ZY::MIN_BIT_LEN, 3);
-    // }
-
     #[test]
     fn struct_roundtrip() {
         #[derive(BitRepr, PartialEq, Debug)]
